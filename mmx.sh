3632548f8e9c715a3635279e30df39e7301f6a8e
XBar <48904834+Xba5@users.noreply.github.com>
2019-04-09 09:06:04 +0000

    Add files via upload

---
+++ mmx.sh
@@ -0,0 +1,12 @@
+echo "mmx part 1"
+sudo apt update
+sudo apt list --upgradable
+sudo apt upgrade
+echo "mmx part 2"
+sudo apt-get install net-tools
+sudo apt list --upgradable
+echo "mmx part 3"
+sudo apt-get install git
+sudo apt-get install toilet
+echo "mmx finished!"
+
